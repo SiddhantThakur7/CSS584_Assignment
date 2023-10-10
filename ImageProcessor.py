f47ed300437070c915cd17136d748e36f2cafbc4
SiddhantThakur7 <siddhant.thakur7@gmail.com>
2023-10-09 20:01:22 -0700

    Added documentation for image_retrieval system and image processor modules.

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,50 +1,88 @@
-import cv2
+import os
 import numpy as np
+import cv2
 import multiprocessing as mp
 import json
-import os
 
+# Number of CPU cores available for multi-processing
 N = mp.cpu_count()
 
 
 def binary_of(x, bits=8):
+    """Converts decimal to sepcified bits binary.
+
+    Args:
+        x: integer
+        bits (int, optional): Number of bits required in the binary equivalent. Defaults to 8.
+
+    Returns:
+        str: binay representation of the decimal number.
+    """
     return format(x, f"0{bits}b")
 
 
 def int_of(x):
+    """Converts binary to decimal bits binary.
+
+    Args:
+        x: string form binary representation.
+
+    Returns:
+        int: decimal conversion of a binary string.
+    """
     return int(x, 2)
 
 
 COLOR_CODE = "color_code"
 INTENSITY = "intensity"
 CACHE_PATH = ".\\cache\\representations.json"
+METHOD_MAP = {"Color": COLOR_CODE, "Intensity": INTENSITY}
 
 
 class ImageProcessor:
     def __init__(self) -> None:
+        # Dictionary containing image representations, histograms and general data
         self.images = {}
-        self.methods = {"Color": COLOR_CODE, "Intensity": INTENSITY}
+
+        # BGR coefficients for intensity representation calculation
         self.INTENSITY_COEFFICIENT_MATRIX = np.array([[0.114], [0.587], [0.299]])
+
+        # Default image order
         self.default_image_list = []
 
         self.initialize()
 
     def initialize(self):
+        """ Verifies the presence of cache data and generates data in its absence"""
         cache_data = self.load_cache_data()
         if cache_data:
             self.images = cache_data
         else:
+            # Process pool spanning across all the available CPU cores 
             with mp.Pool(processes=N) as p:
+                # Aggregate mutlti-processing results
                 results = p.map(self.intialize_image_data, [x for x in range(1, 101)])
             for idx, val in enumerate(results):
                 self.images[idx + 1] = val[idx + 1]
+            # Generate the intensity histogram
             self.process_histograms("intensity")
+            # Generate the Color Code histogram
             self.process_histograms("color_code")
+            # Write the generated data to cache
             self.write_to_cache(self.images)
 
+        # Initialize default image order
         self.default_image_list = list(self.images.values())
 
     def intialize_image_data(self, i):
+        """Generates an image information object containing basic info, representations and histograms of the image.
+
+        Args:
+            i (int): image number. 
+
+        Returns:
+            Dictionary: image basic info(resolution, name), representations with respect to methods(intensity, color-code), and corresponding histograms.
+        """
         image_info = {}
         filepath = f".\\images\\png\\{i}.png"
         image = cv2.imread(filepath, cv2.IMREAD_COLOR)
@@ -67,18 +105,38 @@ class ImageProcessor:
         return image_info
 
     def load_cache_data(self):
+        """Loads data from cache.
+
+        Returns:
+            Dictionary: deserialized cache data.
+        """
         if not os.path.exists(CACHE_PATH):
             return
 
         cache_data = json.load(open(CACHE_PATH, "r"))
+        # Convert data to work within the context of the module
         return self.deserialize_image_data(cache_data)
 
     def write_to_cache(self, data):
+        """Write data to cache.
+
+        Args:
+            data(Dictionary): data to be cached. 
+        """
         json.dump(self.serialize_image_data(data), open(CACHE_PATH, "w+"))
 
     def serialize_image_data(self, images):
+        """Pick and convert the data to JSON serializable format.
+
+        Args:
+            images(Dictionary): List of images with their info.
+
+        Returns:
+            Dictionary: Data contatining image info(name, resolution and histograms).
+        """
         data = {}
         for image in images:
+            # Image representations are not cahced as they are of no interest post histogram computation
             data[image] = {}
             data[image]["name"] = images[image]["name"]
             data[image]["resolution"] = images[image]["resolution"]
@@ -92,6 +150,14 @@ class ImageProcessor:
         return data
 
     def deserialize_image_data(self, images):
+        """Convert the cache data to usable form.
+
+        Args:
+            images(Dictionary): List of images with their info.
+
+        Returns:
+            Dictionary: Image data with deserialized histogram arrays.
+        """
         data = images
         for image in images:
             data[image]["intensity_histogram"] = np.array(
@@ -103,6 +169,16 @@ class ImageProcessor:
         return data
 
     def get_color_code_representaion(self, image, m, n):
+        """Get 6 bit color code representation of an image.
+
+        Args:
+            image (Array): 3D array representation for a 24 bit image.
+            m (Integer): resolution coordinate 1.
+            n (Integer): resolution coordinate 2.
+
+        Returns:
+            list: Array of 6 bit color code calculations.
+        """
         color_code_array = [[0 for _ in range(n)] for _ in range(m)]
         for i in range(m):
             for j in range(n):
@@ -112,7 +188,16 @@ class ImageProcessor:
                 color_code_array[i][j] = int_of(significant_bits)
         return color_code_array
 
-    def calculate_histogram(self, representation, type="intensity"):
+    def compute_histogram(self, representation, type="intensity"):
+        """Computes the histogram with respect to the type passed.
+
+        Args:
+            representation (list): image representation with respect to specific method.
+            type (str, optional): Method used for color histogram calculation. Defaults to "intensity".
+
+        Returns:
+            list: color histogram of a specific image .
+        """
         hist = [0] * (25 if type == "intensity" else 64)
         for i in range(len(representation)):
             for j in range(len(representation[0])):
@@ -127,17 +212,41 @@ class ImageProcessor:
         return np.array(hist)
 
     def process_histograms(self, type="intensity"):
+        """Processes and saves histograms in-memory for all images. 
+
+        Args:
+            type (str, optional): Method used for color histogram calculation. Defaults to "intensity".
+        """
         for image in self.images:
-            self.images[image][f"{type}_histogram"] = self.calculate_histogram(
+            self.images[image][f"{type}_histogram"] = self.compute_histogram(
                 self.images[image][f"{type}_representation"], type
             )
 
     def caclulate_distance(self, image1, image2, type="intensity"):
+        """Calculates manhattan distance between two image histograms.
+
+        Args:
+            image1 (list): image1 representation.
+            image2 (list): image2 representation.
+            type (str, optional): Method used for color histogram calculation. Defaults to "intensity".
+
+        Returns:
+            Integer: manhattan distance betweeen the two images.
+        """
         im1 = image1[f"{type}_histogram"] / image1["resolution"]
         im2 = image2[f"{type}_histogram"] / image2["resolution"]
         return np.sum(np.abs(im1 - im2))
 
     def process_image_distances(self, chosen_image, type="intensity"):
+        """Process image distances between 1 chosen image and all other images.
+
+        Args:
+            chosen_image (list): selected image number.
+            type (str, optional): Method used for color histogram calculation. Defaults to "intensity".
+
+        Returns:
+            list: images distance list. 
+        """
         image_distances = []
         for image in self.images:
             if chosen_image != image:
@@ -152,7 +261,16 @@ class ImageProcessor:
         return image_distances
 
     def retrieve_similar_images(self, chosen_image, method_label):
-        method = self.methods[method_label]
+        """Sorts the image distance pairs between a selected image and all other images.
+
+        Args:
+            chosen_image (list): selected image number.
+            method_label (str): Method used for color histogram calculation.
+
+        Returns:
+            list: image list sorted on lowest to highest distance. 
+        """
+        method = METHOD_MAP[method_label]
         distances = self.process_image_distances(chosen_image, method)
         distances.sort(key=lambda x: x["distance"])
         return distances
